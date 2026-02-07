7ad024a81ce2c3b466870ff1c17fd929b509814a
iliaG <ilia.gogava141@gmail.com>
2026-02-07 23:25:55 +0400

    new

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
